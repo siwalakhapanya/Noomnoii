2f14f1774df65b68c10df7f7fab66fda928f968e
siwalasnru <gtt441@hotmail.com>
2017-07-25 10:58:54 +0700

    เริ่มต้น Branch "HelloWorldApp10time ให้แสดง HelloWorld! 1  ถึง HelloWorld! 10 ที่หน้าจอ/

--- HelloWorld .java
+++ HelloWorld .java
@@ -6,5 +6,9 @@ class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
        // Temporary comments
+        int i = 1;
+        while (i < 11){
+        	System.out.println("HelloWorld" + i);
+        }
     }
 }
